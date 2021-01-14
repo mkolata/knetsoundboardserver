6d69feab867e1e6fc6d4972f65756621d09821ab
Andreas M. Reumschuessel <andreas@reumschuessel.de>
2021-01-14 01:57:15 +0100

    Add simple form data endpoint

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+
+const upload = multer({
+    dest: 'uploads/'
+});
+
 const app = express();
 const port = 3000;
 
+app.use(cors());
+
+app.post('/upload-mp3', upload.single('soundfile'), function (req, res, next) {
+    
+    if (!req.file || req.file.length === 0) {
+        return res.status(400).send('No files were uploaded.');
+      }
+    res.status(201).send();
+})
+
 app.get('/', (req, res) => {
-    res.send('Hello Darling!');
+    res.send('Hello Soundboard!');
 });
 
 app.listen(port, () => {
